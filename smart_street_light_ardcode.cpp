b95386df32dd2b59e65c6c65008c1d4a2bf20d71
rhythmgoel16 <rythm.16783@stu.upes.ac.in>
2024-07-18 15:34:33 +0530

    Update smart_street_light_ardcode.cpp

--- smart_street_light_ardcode.cpp
+++ smart_street_light_ardcode.cpp
@@ -1,22 +1,27 @@
-const int LDRPin = A0; // Analog pin for LDR sensor
-const int LEDPin = 9;  // Digital pin for LED
-int LDRValue = 0;      // Variable to store LDR value
-
-void setup() {
-  pinMode(LEDPin, OUTPUT); // Set LED pin as output
-  Serial.begin(9600);      // Start serial communication for debugging
+int LDRPin=A0; // defining the LDR sensor's pin to read analog values
+int LEDPin=8; // defining the LED pin to receive an output
+int threshold=200; // setting a certain value after which the LED will turn on
+void setup() // setting up the required inputs , outputs and beginning the serial communication
+{
+Serial.begin(9600); // making serial communication with the LDR sensor at 9600 beats per second
+pinMode(LEDPin,OUTPUT); // defining the LED pin to give output
 }
+void loop() 
+{
+  int data=analogRead(LDRPin); // taking the analog values given by the LDR and storing them in a variable
+//printing the values received from the LDR sensor
+Serial.println("");   
+Serial.print("Light Sensor");
+Serial.print("value=");
+Serial.print(data);
+delay(1000); // giving a delay of 1 second in between printing the values
 
-void loop() {
-  LDRValue = analogRead(LDRPin); // Read the LDR value
-  Serial.println(LDRValue);      // Print the LDR value to the serial monitor
-
-  // If ambient light is low (i.e., it is dark), turn on the LED
-  if (LDRValue < 500) {
-    digitalWrite(LEDPin, HIGH);  // Turn on LED
-  } else {
-    digitalWrite(LEDPin, LOW);   // Turn off LED
-  }
-
-  delay(1000); // Wait for a second before repeating the loop
+if(data>=threshold) // if the reading given by the sensor is higher than a certain amount then the light will turn on
+{
+  digitalWrite(LEDPin,HIGH); //LED turns on 
+}
+else // if the reading given by the sensor is lower than a certain amount then the light will remain off
+{
+  digitalWrite(LEDPin,LOW); // light remains off
+}
 }
